2d1235ad81c46d53ca481090c1af16b5d7611000
alvaro-at-xanadu <alvaro@xanadu.ai>
2021-10-04 16:05:44 -0400

    first commit

---
+++ Sample notebook.py
@@ -0,0 +1,35 @@
+r"""
+
+Photonic Quantum Computers
+===========================
+
+The purpose of this section is to introduce how quantum computers may use photons to work.
+Photons are good because they do not interact with each other (longer decoherence times)
+and can be easily transported using optical fibres. Also, we do not have the problem of
+storing at low temperatures. 
+
+"""
+
+##############################################################################
+#
+# Coherent states
+# ----------------
+# A coherent state is a superposition of Fock states. It looks like this:
+# :math:`e^{-|\alpha|^2/2}\sum_{n=0}\frac{\alpha^n}{\sqrt{n!}}\left\lvert n \right\rangle`
+
+import pennylane as qml
+from pennylane import numpy as np
+
+dev = qml.device('default.mixed', wires=2)
+
+@qml.qnode(dev)
+def circuit():
+    qml.Hadamard(wires=0)
+    qml.CNOT(wires=[0, 1])
+    return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
+
+
+print(f"QNode output = {circuit():.4f}")
+
+
+
